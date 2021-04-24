e3c2dc31d7a8390b87b08d3fdf5481c534b68c13
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-24 02:21:14 -0200

    Update Shares.py

--- Shares.py
+++ Shares.py
@@ -6,29 +6,46 @@
 from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 
-API_Key='(KEY)'
+API_Key='KY74URGMWMKH6FJ8'
 ts=TimeSeries(key=API_Key,output_format='pandas')
 
 def getcloseprice():
-	ticket=share
-	data, meta_data=ts.get_daily_adjusted(ticket)
+	ticker=share
+	data, meta_data=ts.get_daily_adjusted(ticker)
 	return(data['4. close'].head(7))
 
-print('OLÁ, FAVOR INSERIR O TICKET DA AÇÃO E CLICAR EM ENTER')
+def descriptor():
+	ticker=share
+	company=''
+	if ticker=='B3SA3.SAO':
+		company='Brasil, Bolsa Balcão'
+	if ticker=='PETR4.SAO':
+		company='Petrobras'
+	else:
+		company=ticker
+	print('O ticker '+ticker+' é da empresa '+company)
+
+def share_on/off():
+	
+
+print('OLÁ, FAVOR INSERIR O TICKER DA AÇÃO E CLICAR EM ENTER')
 
 share=''
 try:
-	ticket=input()
+	ticker=input()+'.SAO'
 	print(getcloseprice())
+	descriptor()
 except:
 	ticket=share
 	n=0
 	while n<1:
 		share='B3SA3.SAO'
 		print(getcloseprice())
+		descriptor()
 		n=n+1
 	n=0
 	while n<1:
 		share='PETR4.SAO'
 		print(getcloseprice())
+		descriptor()
 		n=n+1
