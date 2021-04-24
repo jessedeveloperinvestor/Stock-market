1757e03b9c426eea9b595dea918bff5ad9d9e632
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-24 01:34:52 -0200

    Update Shares.py

--- Shares.py
+++ Shares.py
@@ -6,31 +6,29 @@
 from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 
-API_Key='ALPHA_VANTAGE_KEY'
+API_Key='(KEY)'
 ts=TimeSeries(key=API_Key,output_format='pandas')
 
-def getprices():
+def getcloseprice():
 	ticket=share
 	data, meta_data=ts.get_daily_adjusted(ticket)
-	pprint(data.head(7))
-#	pprint(data['0. date'].head(7))
-	pprint(data['4. close'].head(7))
+	return(data['4. close'].head(7))
 
 print('OLÁ, FAVOR INSERIR O TICKET DA AÇÃO E CLICAR EM ENTER')
 
 share=''
 try:
 	ticket=input()
-	getprices()
+	print(getcloseprice())
 except:
 	ticket=share
 	n=0
 	while n<1:
 		share='B3SA3.SAO'
-		getprices()
+		print(getcloseprice())
 		n=n+1
 	n=0
 	while n<1:
 		share='PETR4.SAO'
-		getprices()
+		print(getcloseprice())
 		n=n+1
