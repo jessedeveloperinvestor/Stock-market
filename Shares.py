7fd526034cd4a261884944e4337e50cca98de03e
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-25 16:47:43 -0200

    Update Shares.py

--- Shares.py
+++ Shares.py
@@ -9,21 +9,40 @@ import time
 import datetime
 import json
 
-stock_ticker='B3SA3.SAO'
-API_Key='KY74URGMWMKH6FJ8'
-ts = TimeSeries (key=API_Key, output_format = "pandas")
+#SHARES:
+a1='B3SA3'
+a2='PETR4'
 
-   ### STOCK TIME SERIES > DAILY ADJUSTED ###
-        # Date / Open / High / Low / Close / Adjusted Close / Volume / Dividend / Split
-data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
-        # data_daily['column name'][row number]
-daterow=meta_data['3. Last Refreshed']
+#SHARES' ACTIVATOR:
+b1=1
 
-data_daily_lastOpenPrice = data_daily['1. open'][0]
-data_daily_lastHighPrice = data_daily['2. high'][0]
-data_daily_lastLowPrice = data_daily['3. low'][0]
-data_daily_lastClosingPrice = data_daily['4. close'][0]
-data_daily_lastTradingVolume = data_daily['5. volume'][0]
+
+#SELECT TICKER:
+symbol=a1
+stock_ticker=symbol+'.SAO'
+
+#ALPHA VANTAGE API:
+def alpha_vantage_api():
+	API_Key='KY74URGMWMKH6FJ8'
+	ts = TimeSeries (key=API_Key, output_format = "pandas")
+	data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
+	        # data_daily['column name'][row number]
+	daterow=meta_data['3. Last Refreshed']
+
+	data_daily_lastOpenPrice = data_daily['1. open'][0]
+	data_daily_lastHighPrice = data_daily['2. high'][0]
+	data_daily_lastLowPrice = data_daily['3. low'][0]
+	data_daily_lastClosingPrice = data_daily['4. close'][0]
+	data_daily_lastTradingVolume = data_daily['5. volume'][0]
+	print(stock_ticker,'close price was ',data_daily_lastClosingPrice,' at ',daterow)
+	print(stock_ticker,'preço de fechamento era ',data_daily_lastClosingPrice,' no dia ',daterow)
+
+#ACTIVATOR:
+if b1==1:
+	alpha_vantage_api()
+else:
+	print(symbol,' não está habilitado/ is not active')
+
 
 conn=sqlite3.connect('shares.db')# or (':memory:')
 c=conn.cursor()
@@ -44,15 +63,37 @@ def insert():
 	conn.commit()
 
 def select():
+	c.execute("SELECT * FROM shares WHERE ticker='B3SA3.SAO'")
+	#print(c.fetchall())
+	print(c.fetchone())
+
+def update():
+	g=0
+
+def decidetoinsertorupdate():
+	from alpha_vantage.timeseries import TimeSeries
+	import pandas as pd
+	API_Key='KY74URGMWMKH6FJ8'
+	ts = TimeSeries (key=API_Key, output_format = "pandas")
+	data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
+	daterow=meta_data['3. Last Refreshed']
+	data_daily_lastClosingPrice = data_daily['4. close'][0]
 	c.execute("SELECT * FROM shares")
-	conn.commit()
+	k=list(c.fetchall())
+	y=str(k)
+	stk=str(stock_ticker)
+	dt=str(daterow)
+	if stk in y:
+		if dt in y:
+			print('updating')
+			update()
+	else:
+		print('inserting')
+		insert()
 
-
-
-print(data_daily_lastClosingPrice)
-print(daterow)
 
 #createtable()
+decidetoinsertorupdate()
 #insert()
 #select()
 
