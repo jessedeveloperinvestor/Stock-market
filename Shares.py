0bf0d474c9e499e7cd89a690a30aba971359cd5a
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-24 23:14:23 -0200

    Update Shares.py

--- Shares.py
+++ Shares.py
@@ -7,7 +7,7 @@ from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 
 API_Key='KY74URGMWMKH6FJ8'
-ts=TimeSeries(key=API_Key,output_format='pandas')
+ts=TimeSeries(key=API_Key,output_format='json')
 
 #SHARES/AÇÕES:
 a='B3SA3.SAO'
@@ -15,63 +15,65 @@ b='PETR4.SAO'
 
 def getcloseprice():
 	ticker=share
-	data, meta_data=ts.get_daily_adjusted(ticker)
-	return(data['4. close'].head(7))
+	data=ts.get_daily_adjusted(ticker)
+	#return(data['4. close'].head(7))
+	return(data)
 
-def descriptor():
-	ticker=share
-	company=''
-	if ticker==a:
-		company='Brasil, Bolsa Balcão'
-	if ticker==b:
-		company='Petrobras'
-	else:
-		company=ticker
-	if ticker.type==str:
-		z='está habilitado'
-	if ticker.type==float:
-		z='não está habilitado'
-	print('O ticker '+str(ticker)+z+' e é da empresa '+company)
+# def descriptor():
+# 	ticker=share
+# 	company=''
+# 	if ticker==a:
+# 		company='Brasil, Bolsa Balcão'
+# 	if ticker==b:
+# 		company='Petrobras'
+# 	else:
+# 		company=ticker
+# 	if ticker.type==str:
+# 		z='está habilitado'
+# 	if ticker.type==float:
+# 		z='não está habilitado'
+# 	print('O ticker '+ticker+z+' e é da empresa '+company)
 
-def share_on_off():
-	print("PARA HABILITAR ESTA AÇÃO, ESCREVA 'OK', DESABILITAR 'NÃO OK' OU PODE DEIXAR EM BRANCO")
-	y=input()
-	if y=='OK':
-		ticker=str(ticker)
-	if y='NÃO OK':
-		ticker=float(ticker)
+# def share_on_off():
+# 	print("PARA HABILITAR ESTA AÇÃO, ESCREVA 'OK', DESABILITAR 'NÃO OK' OU PODE DEIXAR EM BRANCO")
+# 	y=''
+# 	y=input()
+# 	try:
+# 		if y=='OK':
+# 			ticker=str(ticker)
+# 		if y=='NÃO OK':
+# 			ticker=float(ticker)
+# 	except:
+# 		 j=0
 
 def list_builder():
+	k=1
 	#getcloseprice()
 	
 	#filter data, postioned to ',' and build list
 def database_manager():
+	k=2
 	#list_builder()
 
 	#if a==b: update
 	#if a!=b: insert
+
+def runner():
+	pprint(getcloseprice())
+	#share_on_off()
+	#descriptor()
+
+#OUTPUT ACTIONS:
 
 print('OLÁ, FAVOR INSERIR O TICKER DA AÇÃO E CLICAR EM ENTER')
 
 share=''
 try:
 	ticker=input()+'.SAO'
-	print(getcloseprice())
-	share_on_off()
-	descriptor()
+	runner()
 except:
-	ticket=share
-	n=0
-	while n<1:
-		share=a
-		print(getcloseprice())
-		share_on_off()
-		descriptor()
-		n=n+1
-	n=0
-	while n<1:
-		share=b
-		print(getcloseprice())
-		share_on_off()
-		descriptor()
-		n=n+1
+	ticker=share
+	share=a
+	runner()
+	share=b
+	runner()
