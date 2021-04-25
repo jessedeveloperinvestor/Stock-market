f69dee5cf831d0919372e6e5acad668bdc2d9df1
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-25 19:17:22 -0200

    Update Shares.py

--- Shares.py
+++ Shares.py
@@ -12,111 +12,164 @@ import json
 #SHARES:
 a1='B3SA3'
 a2='PETR4'
+a3=''
+a4=''
+a5=''
+a6=''
+a7=''
 
 #SHARES' ACTIVATOR:
 b1=1
-
+b2=1
+b3=0
+b4=0
+b5=0
+b6=0
+b7=0
 
 #SELECT TICKER:
 symbol=a1
 stock_ticker=symbol+'.SAO'
+i=0
 
-#ALPHA VANTAGE API:
-def alpha_vantage_api():
-	API_Key='KY74URGMWMKH6FJ8'
-	ts = TimeSeries (key=API_Key, output_format = "pandas")
-	data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
-	        # data_daily['column name'][row number]
-	daterow=meta_data['3. Last Refreshed']
+#RESULTS:
 
-	data_daily_lastOpenPrice = data_daily['1. open'][0]
-	data_daily_lastHighPrice = data_daily['2. high'][0]
-	data_daily_lastLowPrice = data_daily['3. low'][0]
-	data_daily_lastClosingPrice = data_daily['4. close'][0]
-	data_daily_lastTradingVolume = data_daily['5. volume'][0]
+def results():
+	#ALPHA VANTAGE API:
+	def alpha_vantage_api():
+		API_Key='KY74URGMWMKH6FJ8'
+		ts = TimeSeries (key=API_Key, output_format = "pandas")
+		data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
+		        # data_daily['column name'][row number]
+		daterow=meta_data['3. Last Refreshed'][i]
+		data_daily_lastClosingPrice = data_daily['4. close'][i]
 
-#ACTIVATOR:
-if b1==1:
-	alpha_vantage_api()
-else:
-	print(symbol,' não está habilitado/ is not active')
+	#ACTIVATOR:
+	if b1==1:
+		alpha_vantage_api()
+	else:
+		print(symbol,' não está habilitado/ is not active')
 
 
-conn=sqlite3.connect('shares.db')# or (':memory:')
-c=conn.cursor()
+	conn=sqlite3.connect('shares.db')
+	c=conn.cursor()
 
-def createtable():
-	c.execute("""CREATE TABLE shares (
-				ticker text,
-				date integer,
-				price real
-	)""")
-	conn.commit()
+	def createtable():
+		c.execute("""CREATE TABLE shares (
+					ticker text,
+					date integer,
+					price real
+		)""")
+		conn.commit()
 
-def insert():
-	from alpha_vantage.timeseries import TimeSeries
-	import pandas as pd
-	API_Key='KY74URGMWMKH6FJ8'
-	ts = TimeSeries (key=API_Key, output_format = "pandas")
-	data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
-	daterow=meta_data['3. Last Refreshed']
-	data_daily_lastClosingPrice = data_daily['4. close'][0]
-	date=daterow
-	c.execute('''INSERT INTO  shares (ticker, date, price) VALUES (?, ?, ?) ''',(stock_ticker, date, data_daily_lastClosingPrice))
-	conn.commit()
+	def insert():
+		from alpha_vantage.timeseries import TimeSeries
+		import pandas as pd
+		API_Key='KY74URGMWMKH6FJ8'
+		ts = TimeSeries (key=API_Key, output_format = "pandas")
+		data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
+		daterow=meta_data['3. Last Refreshed']
+		data_daily_lastClosingPrice = data_daily['4. close'][i]
+		date=daterow
+		c.execute('''INSERT INTO  shares (ticker, date, price) VALUES (?, ?, ?) ''',(stock_ticker, date, data_daily_lastClosingPrice))
+		conn.commit()
 
-def select():
-	c.execute("SELECT * FROM shares WHERE ticker='B3SA3.SAO'")
-	#print(c.fetchall())
-	print(c.fetchone())
+	def select():
+		c.execute("SELECT * FROM shares WHERE ticker='B3SA3.SAO'")
+		#print(c.fetchall())
+		print(c.fetchone())
 
-def update():
-	try:
+	def update():
+		try:
+			c.execute('SELECT * FROM shares')
+			[print(row) for row in c.fetchall()]
+			c.execute('''UPDATE shares (ticker, date, price) SET value = (?) WHERE value = (?) AND WHERE value = (?)''',(data_daily_lastClosingPrice, data_daily_lastClosingPrice, daterow))
+			conn.commit()
+		except:
+			c.execute('DELETE FROM shares')
+			conn.commit()
+			from alpha_vantage.timeseries import TimeSeries
+			import pandas as pd
+			API_Key='KY74URGMWMKH6FJ8'
+			ts = TimeSeries (key=API_Key, output_format = "pandas")
+			data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
+			daterow=meta_data['3. Last Refreshed']
+			data_daily_lastClosingPrice = data_daily['4. close'][i]
+			date=daterow
+			c.execute('''INSERT INTO  shares (ticker, date, price) VALUES (?, ?, ?) ''',(stock_ticker, date, data_daily_lastClosingPrice))
+			conn.commit()
+			conn.commit
+
+	def lead_to_insert_or_update():
+		from alpha_vantage.timeseries import TimeSeries
+		import pandas as pd
+		API_Key='KY74URGMWMKH6FJ8'
+		ts = TimeSeries (key=API_Key, output_format = "pandas")
+		data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
+		daterow=meta_data['3. Last Refreshed']
+		data_daily_lastClosingPrice = data_daily['4. close'][i]
+		c.execute("SELECT * FROM shares")
+		k=list(c.fetchall())
+		y=str(k)
+		stk=str(stock_ticker)
+		dt=str(daterow)
+		if stk in y:
+			if dt in y:
+				print('updating')
+				update()
+		else:
+			print('inserting')
+			insert()
+
+	def delete_ticker():
+		try:
+			c.execute("DELETE FROM shares WHERE ticker = 'B3SA3.SAO'")
+		except:
+			c.execute('DELETE FROM shares')
+			conn.commit()
+
+	def show_database():
 		c.execute('SELECT * FROM shares')
 		[print(row) for row in c.fetchall()]
-		c.execute('''UPDATE shares (ticker, date, price) SET value = (?) WHERE value = (?) AND WHERE value = (?)''',(data_daily_lastClosingPrice, data_daily_lastClosingPrice, daterow))
-		conn.commit()
-	except:
-		c.execute('DELETE FROM shares')
-		conn.commit()
 
-def lead_to_insert_or_update():
-	from alpha_vantage.timeseries import TimeSeries
-	import pandas as pd
-	API_Key='KY74URGMWMKH6FJ8'
-	ts = TimeSeries (key=API_Key, output_format = "pandas")
-	data_daily, meta_data = ts.get_daily(symbol=stock_ticker, outputsize ='compact')
-	daterow=meta_data['3. Last Refreshed']
-	data_daily_lastClosingPrice = data_daily['4. close'][0]
-	c.execute("SELECT * FROM shares")
-	k=list(c.fetchall())
-	y=str(k)
-	stk=str(stock_ticker)
-	dt=str(daterow)
-	if stk in y:
-		if dt in y:
-			print('updating')
-			update()
-	else:
-		print('inserting')
-		insert()
+	#createtable()
+	lead_to_insert_or_update()
+	#insert()
+	#select()
+	#show_database()
+	#delete_ticker()
 
-def delete_ticker():
-	try:
-		c.execute("DELETE FROM shares WHERE ticker = 'B3SA3.SAO'")
-	except:
-		c.execute('DELETE FROM shares')
-		conn.commit()
+	conn.close()
 
-def show_database():
-	c.execute('SELECT * FROM shares')
-	[print(row) for row in c.fetchall()]
+#REPEAT RESULTS FUNCTION - LOOP OF TICKERS:
+results()
 
-#createtable()
-#lead_to_insert_or_update()
-#insert()
-#select()
-show_database()
-#delete_ticker()
+symbol=a2
+stock_ticker=symbol+'.SAO'
+i=1
+results()
 
-conn.close()
+# symbol=a3
+# stock_ticker=symbol+'.SAO'
+# i=2
+# results()
+
+# symbol=a4
+# stock_ticker=symbol+'.SAO'
+# i=3
+# results()
+
+# symbol=a5
+# stock_ticker=symbol+'.SAO'
+# i=4
+# results()
+
+# symbol=a6
+# stock_ticker=symbol+'.SAO'
+# i=5
+# results()
+
+# symbol=a7
+# stock_ticker=symbol+'.SAO'
+# i=6
+# results()
